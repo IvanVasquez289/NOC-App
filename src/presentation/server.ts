89fac355f15c9d0a2fb1868a39f4ab338fe63ead
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2024-04-05 17:33:46 -0600

    Cron tasks

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,7 +1,17 @@
+import { CronJob } from "cron"
 
 export class Server {
 
     public static start(){
         console.log('Server running...')
+
+        const job = new CronJob(
+            '*/2 * * * * *', // cronTime
+            () => {
+                const date = new Date()
+                console.log('2 seconds', date);
+            }
+        );
+        job.start()
     }
 }
