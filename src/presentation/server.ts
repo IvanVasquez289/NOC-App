b559073a4576d86f02f3f747dd846ac17d5b319f
ivan <ivanjv1234@gmail.com>
2024-05-06 21:56:35 -0600

    feat: guardar logs en multiples datasources

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,6 @@
 import { LogSeverityLevel } from "../domain/entities/log.entity";
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CheckServiceMultiple } from "../domain/use-cases/checks/check-service-multiple";
 import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDatasource } from "../infraestructure/datasources/file-system.datasource";
 import { MongoDatasource } from "../infraestructure/datasources/mongo.datasource";
@@ -9,9 +10,13 @@ import { CronService } from "./cron/cron-service"
 import { EmailService } from "./email/email.service";
 
 
-const LogRepository = new LogRepositoryImplementation( 
-    // new FileSystemDatasource()
-    // new MongoDatasource()
+const FsRepository = new LogRepositoryImplementation( 
+    new FileSystemDatasource()
+)
+const MongoRepository = new LogRepositoryImplementation( 
+    new MongoDatasource()
+)
+const PostgresRepository = new LogRepositoryImplementation( 
     new PostgresDatasource()
 )
 export class Server {
@@ -26,15 +31,15 @@ export class Server {
         //     ['ivanker289@gmail.com','ivanjv1234@gmail.com',]
         // )
 
-        const logs = await LogRepository.getLogs(LogSeverityLevel.medium)
-        console.log(logs)
+        // const logs = await LogRepository.getLogs(LogSeverityLevel.medium)
+        // console.log(logs)
 
         CronService.createJob(
             '*/4 * * * * *',
             () => {
                 const url = 'https://google.com'
-                new CheckService(
-                    LogRepository,
+                new CheckServiceMultiple(
+                    [FsRepository,MongoRepository,PostgresRepository],
                     () =>  console.log(` ${url} is up!`),
                     (error) => console.log(error),
                 ).execute(url);
